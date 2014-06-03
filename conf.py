6b9ec4840c4468c50206db7836bb936b0405d890
longsail <yuanhang.nju@gmail.com>
2014-06-03 15:20:44 +0800

    update

--- conf.py
+++ conf.py
@@ -1,20 +1,14 @@
-# -*- coding: utf-8 -*-
+#!/usr/bin/env/ python
+#coding=utf-8
 '''
-	weibosearch db configuration
-	
-	Modified by lyuxiao
-	on 2013.11.1
-
-
-    @author Jiajun Huang
-    Created on 2013/10/17
+    @author zouyh
+    Created on 2014/05/10
 '''
 import os
 import sys
 try:
 	import yaml
 except ImportError:
-	print 'importerror'
         print >> sys.stderr
 	sys.exit()
 
@@ -45,18 +39,6 @@ PASSWORD_3 = conf_dic['login'][2]['password_3']
 USERNAME_4 = conf_dic['login'][3]['username_4']
 PASSWORD_4 = conf_dic['login'][3]['password_4']
 
-#USERNAME_5 = conf_dic['login'][4]['username_5']
-#PASSWORD_5 = conf_dic['login'][4]['password_5']
-
-#USERNAME_6 = conf_dic['login'][5]['username_6']
-#PASSWORD_6 = conf_dic['login'][5]['password_6']
-
-#USERNAME_7 = conf_dic['login'][6]['username_7']
-#PASSWORD_7 = conf_dic['login'][6]['password_7']
-
-#USERNAME_8 = conf_dic['login'][7]['username_8']
-#PASSWORD_8 = conf_dic['login'][7]['password_8']
-
 
 COOKIE_FILE = conf_dic['cookie_file']
 LOGDB = conf_dic['logdb']['dbname']
@@ -71,9 +53,5 @@ if __name__ == '__main__':
 	print USERNAME_2,PASSWORD_2
 	print USERNAME_3,PASSWORD_3
 	print USERNAME_4,PASSWORD_4
-	#print USERNAME_5,PASSWORD_5
-	#print USERNAME_6,PASSWORD_6
-	#print USERNAME_7,PASSWORD_7
-	#print USERNAME_8,PASSWORD_8
 	print COOKIE_FILE
 	print LOGDB, LOGHOST, LOGUSER, LOGPW
